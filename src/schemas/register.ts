5650760a2941243969476332643e7f6db4e7d76f
Chekwube <chekwube.udeogu@photizzo.com>
2019-06-26 18:34:29 +0100

    fixed register bug

--- src/schemas/register.ts
+++ src/schemas/register.ts
@@ -3,17 +3,6 @@ import { Schema, model } from "mongoose";
 
 export let registerSchema: Schema = new Schema({
 
-  firstName: {
-    type: String,
-    required: true
-  },
-  lastName: {
-    type: String,
-    required: true
-  },
-  isVerified: {
-    type: Boolean, default: false
-  },
   email: {
     type: String,
     required: true
@@ -21,8 +10,5 @@ export let registerSchema: Schema = new Schema({
   password: {
     type: String,
     required: true
-  },
-  phoneNumber: {
-    type: String
   }
 }, { timestamps: true});
